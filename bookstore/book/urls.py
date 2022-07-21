98187d34e06d04712f998e8146624d0e12b0a6de
wangxd <1508464426@qq.com>
2022-07-21 15:39:21 +0800

    转换器的使用

---
+++ bookstore/book/urls.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+"""
+Author: wangxd
+Date: 2022/7/21
+"""
+from django.urls import path
+from book.views import shop
+from django.urls.converters import register_converter
+
+
+# 自定义转换器
+class MobileConverter:
+    # 验证没有问题的数据，给视图函数
+    regex = '1[3-9]\d{9}'
+
+    def to_python(self, value):
+        return value
+
+
+# 先注册转换器，才能在后面使用
+register_converter(MobileConverter, 'phone')
+
+urlpatterns = [
+    # <转换器名字:变量名>
+    # 转换器会对变量数据进行正则校验
+    path('<int:city>/<phone:mobile>/', shop)
+]
--- bookstore/book/views.py
+++ bookstore/book/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
 
 # Create your views here.
+def shop(request, city, mobile):
+    print(request.META)
+    response = HttpResponse('OK')
+    response['city'] = city
+    response['mobile'] = mobile
+    return response
+    # pass
